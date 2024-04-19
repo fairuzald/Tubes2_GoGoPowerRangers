7dd43ba42ddefcd9d34d8dad1ec7fe21097d1e03
Amalia Putri <amaliaputriii2104@gmail.com>
2024-04-19 11:26:42 +0700

    feat: add button, navbar, logo, cards

--- src/packages/frontend/app/page.tsx
+++ src/packages/frontend/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import InputQuery from "@/components/input-query";
 import { useQueryContext } from "@/components/query-provider";
+import Hero from "@/components/hero";
+import SearchButton from "@/components/search-button";
+import CardGrid from "@/components/individual-paths";
 
 export default function Home() {
   const { state } = useQueryContext();
   console.log(state);
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 w-full">
+    <main className="flex min-h-screen flex-col items-center px-24 py-12 w-full bg-[#14213d] gap-10">
+      <Hero />
       <InputQuery />
+      <SearchButton />
+      <CardGrid/>
     </main>
   );
 }
